09a2ed2b22ea5d3aa5d13e74294195c2d148f2fc
Remida95 <remidallet@hotmail.fr>
2026-09-24 00:35:17 +0200

    Suppression de la partie telechargement des donnees et enregistrement en csv

--- collecte.py
+++ collecte.py
@@ -14,50 +14,6 @@ hier = (date.today() - timedelta(days=1)).isoformat()
 
 url = "https://opendata.reseaux-energies.fr/api/records/1.0/search/"
 
-fichier = "eco2mix_national.csv"
-
-
-"""
-with open(fichier, "w", newline="", encoding="utf-8") as fichier_csv:
-
-    ecrivain = None
-
-    parametres = {
-        "dataset": "eco2mix-national-tr",
-        "rows" : 10000,
-        "start": 0
-    }
-
-    response = requests.get(url, params=parametres)
-    response.raise_for_status()
-
-    donnee = response.json()
-
-    records = donnee["records"]
-
-    #On récupère les clés présent dans fields pour s'en servir comme nom de colonne
-    colonnes = list(records[0]["fields"].keys())
-
-    print("\nColonnes récupérées :")
-    for i in colonnes:
-        print("-", i)
-
-    ecrivain = csv.DictWriter(
-        fichier_csv,
-        fieldnames=colonnes
-    )
-
-    #On écrit les nom des colonnes
-    ecrivain.writeheader()
-
-    # Écriture des données
-    for i in records:
-        if (i.get("fields")).get("consommation") is not None:
-            ecrivain.writerow(i["fields"])
-
-print("\nTerminé !")
-print("Fichier créé :", fichier)"""
-
 # On se connecte à PostgreSQL en récupérant les identifiants depuis les variables chargées
 connexion = psycopg2.connect(
     host=os.getenv("DB_HOST"),
