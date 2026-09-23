d0bf7f54c1e8ff9a13433caff6b0e0ef5b9a6e98
Remida95 <remidallet@hotmail.fr>
2026-09-23 21:16:20 +0200

    Connexion à une base en ligne via neon

--- collecte.py
+++ collecte.py
@@ -62,7 +62,8 @@ connexion = psycopg2.connect(
     port=os.getenv("DB_PORT"),
     dbname=os.getenv("DB_NAME"),
     user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD")
+    password=os.getenv("DB_PASSWORD"),
+    sslmode="require"
 )
 
 print("Connexion à PostgreSQL réussie !")
